dd41618be00b6cbaab997af84a72c3660bffe26e
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-07-08 02:21:15 +0530

    Add files via upload

---
+++ workon.py
@@ -0,0 +1,12 @@
+
+nested_d = {'Beijing':{'China':51, 'USA':36, 'Russia':22, 'Great Britain':19}, 'London':{'USA':46, 'China':38, 'Great Britain':29, 'Russia':22}, 'Rio':{'USA':35, 'Great Britain':22, 'China':20, 'Germany':13}}
+
+US_count = []
+
+
+for i in nested_d.values():
+    print(i)
+    for j in i.keys():
+        if j=='USA':
+            US_count.append(i[j])
+print(US_count)
