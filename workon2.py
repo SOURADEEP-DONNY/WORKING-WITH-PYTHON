2857bc7656e4800d5a4b88a34c7a0d5c57e1ccb6
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-07-08 02:19:05 +0530

    Add files via upload

---
+++ workon2.py
@@ -0,0 +1,7 @@
+third=[]
+p=''
+l_of_l = [['purple', 'mauve', 'blue'], ['red', 'maroon', 'blood orange', 'crimson'], ['sea green', 'cornflower', 'lavender', 'indigo'], ['yellow', 'amarillo', 'mac n cheese', 'golden rod']]
+for i in l_of_l:
+    third.append(i[2])
+    
+print(third)
