527554dda8614a1c32beb274cb32fc0898e3086e
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-07-08 23:20:32 +0530

    Add files via upload

---
+++ workingon3.py
@@ -0,0 +1,6 @@
+first_names=[]
+people = [('Snow', 'Jon'), ('Lannister', 'Cersei'), ('Stark', 'Arya'), ('Stark', 'Robb'), ('Lannister', 'Jamie'), ('Targaryen', 'Daenerys'), ('Stark', 'Sansa'), ('Tyrell', 'Margaery'), ('Stark', 'Eddard'), ('Lannister', 'Tyrion'), ('Baratheon', 'Joffrey'), ('Bolton', 'Ramsey'), ('Baelish', 'Peter')]
+first_names=[i[0] for i in people]
+print(first_names)
+first_names=[i[1] for i in people]
+print(first_names)
