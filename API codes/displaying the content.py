df4d130e22ae55245bb17e570cffbb1c94df1869
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-07-10 13:41:59 +0530

    Add files via upload

---
+++ API codes/displaying the content.py
@@ -0,0 +1,17 @@
+#API URL.
+#TO MAKE A REQUEST TO THE SERVER AND GET A REQUEST FROM THE SERVER
+#USE 'resquest' MODULE
+
+import requests
+url="https://reqres.in/api/users?page=2"
+
+#TO MAKE A REQUEST SEND GET REQUEST.
+
+result=requests.get(url)
+
+#JUST PRINTING THE RESPONSE
+print(result)
+
+print('\n\n')
+#DISPLAYING THE CONTENT
+print(result.content)
