db23a44d2aad5263bf290d6227cd26455e99f66a
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-07-11 14:13:15 +0530

    Add files via upload

---
+++ OBJECT ORIENTED PROGRAMMING IN PYTHON/CREATING A CLASS.py
@@ -0,0 +1,19 @@
+class myid:
+    pass   # PASS KEYWORD IS USED TO DEFINE AN EMPTY CLASS.
+
+#INSTANCE OR OBJECT OF A CLASS
+
+name=myid()
+roll=myid()
+email=myid()
+
+
+
+
+name.candidate='SOURADEEP DAS'
+roll.candidate='ECE2017/003'
+email.candidate='souradeepdoni@gmail.com'
+
+
+print(name.candidate)
+print(email.candidate)
