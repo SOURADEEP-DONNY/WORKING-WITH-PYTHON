7ebc2160d8119b8506eca389ca5c5643f9585b54
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-07-21 22:48:24 +0530

    Add files via upload

--- REGULAR EXPRESSIONS/1.py
+++ REGULAR EXPRESSIONS/1.py
@@ -1,3 +1,11 @@
 import re
+
+#SEARCH KEYWORD
 print(re.search("Donny","Donny is an engineer"))
 print(re.search("table tennis","Donny plays table tennis."))
+
+
+#MATCH KEYWORD
+print(re.match('Donny',"Donny is an engineer"))
+print(re.match("table tennis","Donny plays table tennis."))
+print(re.match('a',"abcd"))
