394517ae228a679b8a6132dbd46090f6828e5929
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-08-11 00:47:34 +0530

    Add files via upload

--- Codevita/DIVISORS IN 1 LINE.py
+++ Codevita/DIVISORS IN 1 LINE.py
@@ -8,3 +8,44 @@ if N>=1 and N<=10**8:
     store.sort()
     for i in store:
         print(i,end=' ')
+
+
+print('\n\n')
+# THE ABOVE CODE WILL NOT RUN IN ANY COMPETITIVE EXAM AND WILL GIVE A RUNTIME
+# ERROR.
+
+N=int(input())
+if N>=1 and N<=10**8:
+    store=[]
+
+    for i in range(1,int(N**0.5)+1):
+        if N%i==0:
+            if i*i==N:
+                store.append(i)
+            else:
+                store.append(i)
+                store.append(N//i)
+
+        store.sort()
+    for i in store:
+        print(i,end=' ')
+
+
+print('\n\n')
+
+
+# ANOTHER SLOTUION
+N=int(input())
+store=[]
+if N>=1 and N<=10**8:
+   
+
+    for i in range(1,int(N**0.5)+1):
+        if N%i==0:
+            if i*i==N:
+                print(i,end=' ')
+            else:
+                print(i,end=' ')
+                store.insert(0,N//i)
+    for i in store:
+        print(i,end=' ')
