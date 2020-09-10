417793c6f242f6c7562f4716c4ebcff8f74cae8b
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-09-10 17:13:30 +0530

    Add files via upload

--- Dictionary Comprehensions/2.py
+++ Dictionary Comprehensions/2.py
@@ -1,7 +1,7 @@
 #Storing Salary of different people in a dictionary.
 
 dict1={}
-for i in range(5):
+for i in range(3):
     n=input("\nEnter a name:")
     s=int(input("\nEnter salary"))
     dict1[n]=s
@@ -9,3 +9,11 @@ for i in range(5):
 print(dict1)
 
           
+
+
+# Changing a list of tuples into a dictionary
+
+lst=[(1,'Soura'),(2,'Deep'),(3,'Donny')]
+dict1={k:v for k,v in lst}
+
+print(dict1)
