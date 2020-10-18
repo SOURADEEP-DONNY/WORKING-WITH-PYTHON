5ca9ea739cbf1597367b435f05d0de700452d636
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-10-18 20:39:38 +0530

    Add files via upload

---
+++ 14.py
@@ -0,0 +1,33 @@
+if ('-1'):
+    print('ouch')
+else:
+    print('echo')
+
+def x(y):
+    pass
+
+x = 4
+y = 5
+if (x == 3):
+    print( "This is it")
+elif (y<12):
+    print("South")
+elif (y>12):
+    print("North")
+elif (x == 4):
+    print("East")
+else:
+    print("West")
+
+if (x < 2):
+    print("1") 
+elif (x < 6):
+    print("2") 
+elif (x < 4):
+    print("3") 
+elif (x > 4):
+    print("4") 
+elif (x < 10):
+    print("5")
+else:
+    print("6")
