95a45f01cbfb480cd80cc06377808cf8be4d0a73
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-12-03 11:39:13 +0530

    Add files via upload

--- 2.py
+++ 2.py
@@ -1,20 +1,11 @@
-n=input("\nEnter a number")
-ad=0
-list1=[]
-for i in n:
-    list1.append(i)
-k=list1[-1]
-k=int(k)
-
-for i in list1:
-    i=int(i)
-    ad=ad+(i**k)
-    
-
-##print(ad)
-n=int(n)
-if ad==n:
-    print(True)
-else:
-    print(False)
-    
+T=int(input())
+n=int(input())
+s=input()
+if T>=1 and T<=10:
+    if n>=1 and n<=100000:
+        s=list(s)
+        r=1
+        for i in range(n-2):
+            if s[i]=='0' and s[i+1]=='1' and s[i+2]=='0':
+                r=r+1
+        print(r)
