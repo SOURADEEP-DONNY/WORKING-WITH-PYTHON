f66f61bbebf0a2ddf8c1a1309860e24561a5fcf5
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-12-04 23:33:20 +0530

    Add files via upload

---
+++ adas.py
@@ -0,0 +1,13 @@
+f=open("aaa.txt","r")
+s=f.read()
+l=len(s)
+word=l.split(" ")
+print(word)
+#i=0
+#while i<l:
+#    if s[i] not in temp:
+#        temp=temp+s[i]
+#        t=split
+#        print(s[i],':',t)
+#        i=i+1
+    
