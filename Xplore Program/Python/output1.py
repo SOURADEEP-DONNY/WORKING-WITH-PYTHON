4ce7e6d38aaa62da45c2207fc9e7e494fd0c0b59
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2021-01-14 11:32:15 +0530

    Add files via upload

---
+++ Xplore Program/Python/output1.py
@@ -0,0 +1,14 @@
+si='spamandeggs'
+x=si.find('and')
+print(x)
+print(si[0:1],si[5:7])
+
+'''print("%.2f%123.444")'''
+nums=set([1,1,2,3,3,3,4])
+print(len(nums))
+
+str1='hello'
+print(str1[-1:])
+
+print('new"line')
+print('new''line')
