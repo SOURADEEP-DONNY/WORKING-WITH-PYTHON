b0eb22a16dc2d1b35fcc6f68ecbd65facc6aab20
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2021-01-16 14:28:35 +0530

    Add files via upload

---
+++ Xplore Program/Python codes/oop1.py
@@ -0,0 +1,11 @@
+class Person:
+    def __init__(self,first_name,last_name):
+        self.first_name=first_name
+        self.last_name=last_name
+
+'''Creating objects'''
+p1=Person('Souradeep','Das')
+p2=Person('Abhinandan','Das')
+
+
+print(p1.first_name,p1.last_name)
