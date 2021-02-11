6fce70ed0a7886dbbbcc0ee5f84d58bd24984799
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2021-02-11 12:17:45 +0530

    Add files via upload

--- List Comprehension/1.py
+++ List Comprehension/1.py
@@ -2,3 +2,10 @@ l1=[int(i) for i in input().split()]
 print(l1)
 l2=[int(i) for i in range(10)]
 print(l2)
+
+list3=[int(i) for i in range(20) if i%2==0 if i%5==0] #Interprets AND logic
+print(list3)
+
+# USING if-else STATEMENTS TOEGTHER
+list4=[int(i) if i%2==0 else 'odd' for i in range(10)]
+print(list4)
