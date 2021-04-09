9ab7fe3b847e210e484074092e93438c237588de
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2021-04-10 01:17:39 +0530

    Add files via upload

---
+++ op7.py
@@ -0,0 +1,5 @@
+scores={'math':45,'english':60,'science':65,'computer science':70}
+del scores['science']
+#scores['science'].remove() --> Attribute Error
+#remove scores['science'] -->Invalid Syntax
+print(scores)
