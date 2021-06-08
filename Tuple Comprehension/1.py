91d52774630a1d9e8339a94f583c7a600b6a2c07
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2021-06-08 19:38:08 +0530

    Add files via upload

--- Tuple Comprehension/1.py
+++ Tuple Comprehension/1.py
@@ -5,6 +5,23 @@ print(t)
 
 tuple1=1,2,3,[5,6,7],48
 print(tuple1)
+
+
 #inserting inside a tuple list
 tuple1[3].append("Insert1")
 print(tuple1)
+
+
+#concatenation of tuples
+print(t+tuple1)
+
+#removing an entire tuple
+del tuple1
+
+#user defined tuple
+l=[]
+n=int(input())
+for i in range(n):
+    l.append(int(input()))
+tup=tuple(l)
+print(tup)
