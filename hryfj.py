f21fedd877a0867ae3a24de9a7fb485ff385e1e1
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2021-07-13 15:51:31 +0530

    Add files via upload

---
+++ hryfj.py
@@ -0,0 +1,5 @@
+import random
+print(int(30+random.random()*5),end=' ')
+#print(int(30+random.random()*5),end=' ')
+#print(int(40+random.random()*5),end=' ')
+#print(int(30+random.random()*5),end=' ')
