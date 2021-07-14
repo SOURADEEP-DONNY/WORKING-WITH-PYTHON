c377ad78eb953b88a68235b933d38e82abecc523
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2021-07-14 13:41:31 +0530

    Add files via upload

---
+++ fun5.py
@@ -0,0 +1,10 @@
+def gt(a,b):
+    if(a>b):
+        return a
+    else:
+        return b
+x=int(input("Enter a number: "))
+y=int(input("Enter another number: "))
+t=gt(x,y)
+print("Greatest number: ",t)
+
