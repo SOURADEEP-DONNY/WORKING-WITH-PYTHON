6ed71f73493e369adc21217cb2bc7745cea0c5a3
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2021-07-18 14:32:19 +0530

    Add files via upload

--- asdasd.py
+++ asdasd.py
@@ -1,8 +1,6 @@
-f=open("Abhi.txt","r")
-s=f.readline()
-count=0
-for v in f:
-    if(v=='a' or v=='e' or v=='i' or v=='o' or v=='u'):
-        count=count+1
-print(count)        
-        
+for i in range(1,11):
+    print(i)
+    if i==5:
+        break
+    print(i)
+
